d205c7f67826e21bed02dc453728fabec36a7a4c
Laura Carvajal <laura.carvajal@gmail.com>
2017-02-20 21:15:47 +0000

    hideElements

--- config/.pa11yci.js
+++ config/.pa11yci.js
@@ -12,25 +12,25 @@ const urls = [];
 const config = {
 	defaults: {
 		page: {},
-		timeout: 50000
+		timeout: 50000,
+		hideElements: 'iframe[src*=google],iframe[src*=proxy]',
 	},
-	hideElements: 'iframe[src*=google],iframe[src*=proxy]',
 	urls: []
 }
 
 // Override with project specifics, if any
 const exceptions = process.env.PA11Y_ROUTE_EXCEPTIONS ? process.env.PA11Y_ROUTE_EXCEPTIONS.split(',') : [];
 config.defaults.page.headers = process.env.PA11Y_HEADERS ? JSON.parse(process.env.PA11Y_HEADERS) : {Cookie: 'next-flags=ads:off,cookieMessage:off; secure=true'};
-config.hideElements = process.env.PA11Y_HIDE ? `${process.env.PA11Y_HIDE},${config.hideElements}` : config.hideElements;
+config.defaults.hideElements = process.env.PA11Y_HIDE ? `${process.env.PA11Y_HIDE},${config.defaults.hideElements}` : config.defaults.hideElements;
 
 console.log('PA11Y_ROUTE_EXCEPTIONS:', process.env.PA11Y_ROUTE_EXCEPTIONS);
 console.log('exceptions:', exceptions);
 console.log('PA11Y_ROUTE_HEADERS:', process.env.PA11Y_ROUTE_HEADERS);
 console.log('headers:', config.defaults.page.headers);
 console.log('PA11Y_HIDE:', process.env.PA11Y_HIDE);
-console.log('config.hideElements:', config.hideElements);
+console.log('config.defaults.hideElements:', config.defaults.hideElements);
 
-// Don't console.log headers once backend key is added to the header object
+// Don't console.log headers once backend key is added to the object
 config.defaults.page.headers['FT-Next-Backend-Key'] = process.env.FT_NEXT_BACKEND_KEY;
 
 
