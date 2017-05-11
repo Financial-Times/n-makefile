9cffc230bf4a4ad0892589f0882809c0070befe2
Laura Carvajal <laura.carvajal@gmail.com>
2017-05-11 14:34:14 +0100

    fix viewports, add viewport name to screenshot, default to smaller viewport

--- config/.pa11yci.js
+++ config/.pa11yci.js
@@ -1,7 +1,9 @@
+const extend = require('node.extend');
+
 const viewports = process.env.PA11Y_VIEWPORTS || [
 	{
-		width: 1440,
-		height: 1220
+		width: 1280,
+		height: 800
 	}
 ];
 
@@ -30,6 +32,7 @@ const config = {
 			}
 		},
 		timeout: 50000,
+		wait: 1000,
 		hideElements: 'iframe[src*=google],iframe[src*=proxy]',
 		rules: ['Principle1.Guideline1_3.1_3_1_AAA']
 	},
@@ -74,10 +77,6 @@ smoke.forEach((smokeConfig) => {
 			url: process.env.TEST_URL + url
 		}
 
-		if (process.env.TEST_URL.includes('local')) {
-			thisUrl.screenCapture = './pa11y_screenCapture/' + url + '.png';
-		}
-
 		// Do we have test-specific headers?
 		if (smokeConfig.headers) {
 			thisUrl.page = {};
@@ -115,9 +114,18 @@ smoke.forEach((smokeConfig) => {
 });
 
 for (let viewport of viewports) {
+
 	for (let url of urls) {
-		url.viewport = viewport;
-		config.urls.push(url);
+
+		const resultUrl = extend(true, {page: {viewport: viewport}}, url);
+
+		if (process.env.TEST_URL.includes('local')) {
+			const path = resultUrl.url.substring(resultUrl.url.lastIndexOf('/'));
+			resultUrl.screenCapture = `./pa11y_screenCapture/${viewport.width}x${viewport.height}/${path || 'root'}.png`;
+
+		}
+
+		config.urls.push(resultUrl);
 	}
 }
 
